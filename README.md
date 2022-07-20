065d82b3077f5afda75352c3f2e90f01f9e19a8a
Jared Tulayan <jared@jaredtulayan.xyz>
2022-07-20 17:46:19 -0400

    Init

---
+++ README.md
@@ -0,0 +1,2 @@
+# Rustbucket
+A spiritual successor to Hold my Coffee, written in Rust.
---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_system(test_system)
+        .run();
+}
+
+fn test_system() {
+    println!("Testing");
+}
