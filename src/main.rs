7b3f8cfd10dc87da0894233862193aee60d0554b
Jared Tulayan <jared@jaredtulayan.xyz>
2022-07-25 00:47:44 -0400

    Add stuff for web build

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,35 @@ mod entities;
 mod systems;
 
 use bevy::prelude::*;
+use bevy::window::PresentMode;
+
 use entities::components::*;
 use systems::systems::*;
 
 fn main() {
-    App::new()
+    let mut app = App::new();
+
+    // Default stuff
+    app
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
         .add_system(move_player)
-        .add_system(lifetime_system)
-        .run();
+        .add_system(lifetime_system);
+
+    app.insert_resource(WindowDescriptor {
+        title: "Rustbucket".to_string(),
+        width: 720.0,
+        height: 1280.0,
+        present_mode: PresentMode::Fifo,
+        ..default()
+    });
+    
+    app.run();
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+
     commands
         .spawn()
         .insert_bundle(PlayerBundle {
